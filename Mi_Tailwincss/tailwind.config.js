7dfcd59cf292679c83f55ddf994a79ee568487ff
Daniel <dosuna981@gmail.com>
2025-02-07 11:10:31 +0100

    Creando una funcion con sass y subiendo archivos de tailwindcss

---
+++ Mi_Tailwincss/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}", // si tienes una carpeta src
+    "./*.html",            // si tienes archivos HTML en la raíz
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
